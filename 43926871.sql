2f4aeabac78a230d01d934f74a70ba6f8d1337b7
Max Bo <max@maxbo.me>
2017-10-20 17:56:52 +1000

    Init

---
+++ 43926871.sql
@@ -0,0 +1,71 @@
+-- INFS2200 Assignment, Semester 2 - 2017
+
+-- Student Number: 43926871
+-- Student Name: Maxwell Bo
+-- DOG_ID: 3376
+
+--#############################################################################
+
+-- Task 1: Constraints
+
+-- a)
+
+-- b)
+
+--#############################################################################
+
+-- Task 2: Triggers
+
+-- a)
+
+-- b)
+
+-- c)
+
+-- d)
+
+--#############################################################################
+
+-- Task 3: Views
+
+-- a)
+
+-- b)
+
+-- c) Note this question also has an explanation component. 
+
+--#############################################################################
+
+-- Task 4: Function Based Indexes
+
+-- a)
+
+-- b)
+
+-- c) Note this question also has an explanation component. 
+
+--#############################################################################
+
+-- Task 5: Bitmap Indexing
+
+-- a)
+
+-- b)
+
+-- c) Note this question also has an explanation component. 
+
+-- d) Note this question also has an explanation component. 
+
+--#############################################################################
+
+-- Task 6: Execution Plan & Analysis
+
+-- b)
+
+-- c) Note this question also has an explanation component. 
+
+-- d) Note this question also has an explanation component. 
+
+-- e)
+
+--#############################################################################
